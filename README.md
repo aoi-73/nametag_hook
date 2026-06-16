f069a0c7954ddf0658214b486a4e5fa957a7895d
ʎɥdɹnꟽ <andr3lr1103@gmail.com>
2026-06-16 14:44:46 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+```bash
+g++ -shared -o nametag.dll dllmain.cpp minhook/src/buffer.c minhook/src/hook.c minhook/src/trampoline.c minhook/src/hde/hde64.c -I./minhook/include -I./minhook/src -std=c++17 -luser32 -Wall
+```
+```
---
+++ dllmain.cpp
@@ -0,0 +1,46 @@
+#include <windows.h>
+#include <iostream>
+#include "minhook/include/MinHook.h"
+
+typedef char(__fastcall* tAddNametagEntity)(__int64 a1, __int64 a2);
+tAddNametagEntity fnOriginalAddNametagEntity = nullptr;
+
+char __fastcall hkAddNametagEntity(__int64 a1, __int64 a2) {
+    // Aquí puedes meter tu lógica de análisis en el futuro
+    return fnOriginalAddNametagEntity(a1, a2);
+}
+
+// 3. Hilo principal de nuestra DLL
+DWORD WINAPI MainThread(LPVOID lpReserved) {
+    if (MH_Initialize() != MH_OK) {
+        return FALSE;
+    }
+
+    DWORD_PTR clientBase = 0;
+    while (clientBase == 0) {
+        clientBase = (DWORD_PTR)GetModuleHandleA("client.dll");
+        Sleep(100);
+    }
+
+    DWORD_PTR targetAddress = clientBase + 0x78CC30;
+
+    if (MH_CreateHook((LPVOID)targetAddress, (LPVOID)&hkAddNametagEntity, (LPVOID*)&fnOriginalAddNametagEntity) == MH_OK) {
+        MH_EnableHook((LPVOID)targetAddress);
+    }
+
+    return TRUE;
+}
+
+extern "C" __declspec(dllexport) BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved) {
+    switch (dwReason) {
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls(hMod);
+            CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+            break;
+        case DLL_PROCESS_DETACH:
+            MH_DisableHook(MH_ALL_HOOKS);
+            MH_Uninitialize();
+            break;
+    }
+    return TRUE;
+}
